f812d57562799ef7d665065ea2c5a92895844ed3
Dzianis Lapanik <dzianis_lapanik@epam.com>
2022-05-25 19:58:26 +0300

    attempt #1

--- cache.go
+++ cache.go
@@ -2,22 +2,59 @@ package cache
 
 import "time"
 
+type cacheRecord struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	data map[string]cacheRecord
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{data: make(map[string]cacheRecord)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	value, ok := c.data[key]
 
+	if !ok {
+		return "", false
+	}
+
+	if value.deadline.Before(time.Now()) {
+		delete(c.data, key)
+		return "", false
+	}
+
+	return value.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.data[key] = cacheRecord{
+		value:    value,
+		deadline: time.Time{}, // zero value for Time
+	}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, len(c.data))
+	now := time.Now()
+
+	for key, value := range c.data {
+		if value.deadline.Before(now) {
+			delete(c.data, key)
+		} else {
+			keys = append(keys, key)
+		}
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.data[key] = cacheRecord{
+		value:    value,
+		deadline: deadline,
+	}
 }
